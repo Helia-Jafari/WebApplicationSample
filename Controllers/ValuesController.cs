3c10ed7e4c2ca1baf2aa5871750e915a46510eb3
dr.heliajafari@gmail.com <dr.heliajafari@gmail.com>
2025-02-04 21:03:41 +0330

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationSample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        public IActionResult Greeting()
+        {
+            string message = "Hi world";
+            return Ok(message);
+        }
+
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,39 @@
+namespace WebApplicationSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+
+            builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.adds
+
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
